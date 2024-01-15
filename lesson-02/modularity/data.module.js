c837a7f3cb1e25b0fb6c587d8eb6c4931ee06b98
Antonio Klenitsky <a_klenitsky@mail.ru>
2024-01-15 23:19:44 +0300

    lesson-02

---
+++ lesson-02/modularity/data.module.js
@@ -0,0 +1,4 @@
+​console.log("data, start"); 
+export let a = 10; 
+let b = 20; 
+console.log("data, finish"); 
---
+++ lesson-02/modularity/main.js
@@ -0,0 +1,6 @@
+
+import { a } from "./data.module.js";
+import { render } from "./render.module.js";
+console.log("main, start"); 
+render(a); 
+console.log("main, finish");
